47cfa19b8024e20225062154b57f8db8206db724
llleoy <1421877537@qq.com>
2022-06-09 17:31:15 +0800

    Create UploadController.cs

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knowledge_Graph_Analysis_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+        {
+            if (file == null)
+            {
+                return BadRequest("There isn't a file.");
+            }
+            string[] limitFileType = { ".csv" };
+            string currentFileExtension = Path.GetExtension(file.FileName).ToLower();
+            if (!limitFileType.Contains(currentFileExtension))
+            {
+                return new JsonResult(new { code = "-1", msg = "File must be csv file." });
+            }
+            var filePath = Path.Combine("imports/", file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok(new { size = file.Length, msg = "successfully upload the file!" });
+        }
+    }
+}
